73a7de49e22c0c34b4c50550af5528dec5e0ddb5
unav4ila8le <leonardobalduzzi@me.com>
2025-02-25 18:12:14 +0900

    added dashboard initial layout

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
   return (
     <div className="flex h-full items-center justify-center">
       <h1 className="text-4xl font-semibold">patrivio</h1>
+      <Button>Get Started</Button>
     </div>
   );
 }
