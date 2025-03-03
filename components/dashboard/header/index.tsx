bdcef15f39630ce5874e2d39f2c059a619919c12
unav4ila8le <leonardobalduzzi@me.com>
2025-03-03 20:58:15 +0900

    sidebar trigger tooltip

--- components/dashboard/header/index.tsx
+++ components/dashboard/header/index.tsx
@@ -1,12 +1,26 @@
+import { Plus } from "lucide-react";
+
 import { SidebarTrigger } from "@/components/ui/sidebar";
 import { ThemeToggle } from "@/components/theme-toggle";
 import { Button } from "@/components/ui/button";
-import { Plus } from "lucide-react";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
 
 export function Header() {
   return (
-    <div className="flex items-center justify-between pt-2 pr-4 pb-0 pl-2">
-      <SidebarTrigger />
+    <div className="flex items-center justify-between px-4 pt-2 pb-0">
+      <TooltipProvider>
+        <Tooltip delayDuration={700}>
+          <TooltipTrigger asChild>
+            <SidebarTrigger className="-ml-2" />
+          </TooltipTrigger>
+          <TooltipContent>Toggle sidebar</TooltipContent>
+        </Tooltip>
+      </TooltipProvider>
       <div className="flex items-center gap-2">
         <Button>
           <Plus />
