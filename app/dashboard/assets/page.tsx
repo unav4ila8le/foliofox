e373bdf5ec492490d9bd7b3862200fbe226470bf
unav4ila8le <leonardobalduzzi@me.com>
2025-04-02 15:27:11 +0900

    table init

--- app/dashboard/assets/page.tsx
+++ app/dashboard/assets/page.tsx
@@ -1,3 +1,31 @@
-export default function Assets() {
-  return <div>Assets</div>;
+import { Payment, columns } from "@/components/dashboard/assets/table/columns";
+import { DataTable } from "@/components/dashboard/assets/table/data-table";
+
+async function getData(): Promise<Payment[]> {
+  // Fetch data from your API here.
+  return [
+    {
+      id: "728ed52f",
+      amount: 100,
+      status: "pending",
+      email: "m@example.com",
+    },
+    // ...
+  ];
+}
+
+export default async function Assets() {
+  const data = await getData();
+
+  return (
+    <div className="flex flex-col gap-4">
+      <div>
+        <h1 className="text-2xl font-semibold">Asset Portfolio</h1>
+        <p className="text-muted-foreground">
+          Here&apos;s a list of all your assets
+        </p>
+      </div>
+      <DataTable columns={columns} data={data} />
+    </div>
+  );
 }
