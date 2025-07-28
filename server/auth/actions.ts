8cc5e4109f49140f57940844f19b43c2b2ab693f
unav4ila8le <leonardobalduzzi@me.com>
2025-07-28 16:25:03 +0900

    update packages and auth actions

--- server/auth/actions.ts
+++ server/auth/actions.ts
@@ -9,17 +9,15 @@ import { createClient } from "@/utils/supabase/server";
 export async function getCurrentUser() {
   const supabase = await createClient();
 
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
+  const { data, error } = await supabase.auth.getUser();
 
   // Redirect to login if no user
-  if (!user) {
+  if (error || !data?.user) {
     redirect("/auth/login");
   }
 
   // Return supabase client and user
-  return { supabase, user };
+  return { supabase, user: data.user };
 }
 
 // Login
