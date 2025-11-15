135c5c4b4389c75ecfb12fa092a656dd2d1b035c
unav4ila8le <leonardobalduzzi@me.com>
2025-11-15 17:14:06 +0900

    update database types

--- types/database.types.ts
+++ types/database.types.ts
@@ -7,6 +7,11 @@ export type Json =
   | Json[]
 
 export type Database = {
+  // Allows to automatically instantiate createClient with right options
+  // instead of createClient<Database, { PostgrestVersion: 'XX' }>(URL, KEY)
+  __InternalSupabase: {
+    PostgrestVersion: "13.0.5"
+  }
   graphql_public: {
     Tables: {
       [_ in never]: never
@@ -881,4 +886,3 @@ export const Constants = {
     },
   },
 } as const
-
