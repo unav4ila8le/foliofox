f8afcf6e573b75c35f986501c6713cb13c760618
unav4ila8le <leonardobalduzzi@me.com>
2025-12-29 15:19:27 +0100

    style: refine global CSS variables and update component styles for consistency
    - Adjusted color values for primary and secondary foregrounds in globals.css to enhance visual coherence.
    - Updated the layout component to apply the primary foreground color for better design alignment.
    - Modified button styles in the Hero component to ensure proper text color in dark mode.
    - Enhanced table styles in the PublicPortfolioAssetsTable component for improved background handling.

--- app/(public)/layout.tsx
+++ app/(public)/layout.tsx
@@ -6,7 +6,7 @@ export default function PublicLayout({
   children: React.ReactNode;
 }) {
   return (
-    <div>
+    <div className="bg-primary-foreground">
       <Header />
       <main>{children}</main>
     </div>
