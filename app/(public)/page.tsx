eca1af5b3682accb32fdf9f63aa38c97994153d5
unav4ila8le <leonardobalduzzi@me.com>
2026-02-22 17:42:03 +0900

    bmc button added on the homepage

--- app/(public)/page.tsx
+++ app/(public)/page.tsx
@@ -1,9 +1,26 @@
+import Link from "next/link";
+import Image from "next/image";
+
 import { Hero } from "@/components/homepage/hero";
 
 export default function HomePage() {
   return (
-    <div className="container mx-auto mt-12 max-w-7xl p-3">
+    <div className="relative mx-auto mt-12 max-w-7xl p-3">
       <Hero />
+
+      {/* BMC Button */}
+      <Link
+        href="https://buymeacoffee.com/leonardofromfoliofox"
+        target="_blank"
+        className="fixed right-8 bottom-8 transition-all duration-100 hover:scale-105"
+      >
+        <Image
+          src="/images/homepage/bmc-button.svg"
+          alt="Buy me a Milk Tea"
+          width={164}
+          height={46}
+        />
+      </Link>
     </div>
   );
 }
