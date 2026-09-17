52fa60368980b10d2e7a5683eea37dde8caacc1e
unav4ila8le <leonardobalduzzi@me.com>
2026-09-17 14:13:40 +0900

    Every shadcn component now imports cn from the cn package

--- components/ui/pagination.tsx
+++ components/ui/pagination.tsx
@@ -1,7 +1,7 @@
 import * as React from "react"
+import { cn } from "cn"
 import Link from "next/link"
 
-import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
 import { ChevronLeftIcon, ChevronRightIcon, MoreHorizontalIcon } from "lucide-react"
 
